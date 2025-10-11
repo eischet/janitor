a9a3b41123081b3a00d1cae039ded8d19140a6d5
Stefan Eischet <stefan@eischet.net>
2025-10-11 16:23:15 +0200

    ignore run configs from IntelliJ

---
+++ janitor-lang/src/test/java/com/eischet/janitor/AllJanitorLangTests.java
@@ -0,0 +1,18 @@
+package com.eischet.janitor;
+
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+/**
+ * JUnit 5 test suite for the janitor-lang module.
+ *
+ * This suite scans the com.eischet.janitor package and includes all test classes
+ * that follow common naming conventions (…Test or …TestCase).
+ */
+@Suite
+@SelectPackages("com.eischet.janitor")
+@IncludeClassNamePatterns({"^.*Test$", "^.*TestCase$"})
+public class AllJanitorLangTests {
+    // Intentionally empty; annotations configure the suite.
+}
