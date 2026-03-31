4c2110621ef5169aa648f3e600fbecf3c693de18
Stefan Eischet <stefan@eischet.net>
2026-03-31 14:06:23 +0200

    brrr module: Message should be a constructor, not property

--- janitor-modules/brrr/src/main/java/com/eischet/janitor/modules/brrr/BrrrModule.java
+++ janitor-modules/brrr/src/main/java/com/eischet/janitor/modules/brrr/BrrrModule.java
@@ -2,6 +2,7 @@ package com.eischet.janitor.modules.brrr;
 
 import com.eischet.janitor.api.modules.JanitorModule;
 import com.eischet.janitor.api.modules.JanitorModuleRegistration;
+import com.eischet.janitor.api.types.builtin.JMap;
 import com.eischet.janitor.api.types.composed.JanitorComposed;
 import com.eischet.janitor.api.types.dispatch.DispatchTable;
 
@@ -11,7 +12,14 @@ public class BrrrModule extends JanitorComposed<BrrrModule> implements JanitorMo
     public static final DispatchTable<BrrrModule> DISPATCH = new DispatchTable<>(BrrrModule::new);
 
     static {
-        DISPATCH.addObjectProperty("Message", self -> new BrrrMessage());
+        DISPATCH.addMethod("Message", (self, process, args) -> {
+            final BrrrMessage message = new BrrrMessage();
+            if (args.size() > 0) {
+                final JMap map = args.getRequired(0, JMap.class);
+                map.applyTo(process, message);
+            }
+            return message;
+        });
     }
 
     public BrrrModule() {
